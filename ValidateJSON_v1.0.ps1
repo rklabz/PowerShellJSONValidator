6bd21aab4fde94eb17d2d99d0e06e1b5256508d3
RK Labz <syed.rehan.kazmi@hotmail.com>
2020-03-12 11:56:49 +0500

    Update ValidateJSON_v1.0.ps1

--- ValidateJSON_v1.0.ps1
+++ ValidateJSON_v1.0.ps1
@@ -1,21 +1,9 @@
-﻿# Restart All Ascent Services
 Write-Host '************ Validating JSON Files ************' -BackgroundColor White -ForegroundColor Blue
 
 $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
- 
-#$FileName = Read-Host "Enter file name to validated: " 
-#$CompleteFileName = $ScriptDir + "\" + $FileName
-
-#$CompleteFileName = $ScriptDir + "\json.txt"
-#Get-Item -Path $CompleteFileName -Stream *
-
-#$FileData = Get-Content -Path $CompleteFileName -Stream $DATA
-
-#Get-Content -Path $CompleteFileName -Stream $DATA
 
 Write-Host "Current directory is $ScriptDir" -ForegroundColor Cyan
 Write-Host "Checking all TXT files in the current directory:" -ForegroundColor Yellow
-#Get-ChildItem -Path $ScriptDir -Name  
 
 Get-ChildItem $ScriptDir -Filter *.txt | 
 Foreach-Object {
@@ -40,7 +28,3 @@ Foreach-Object {
          Write-Host "*******" $_.Name " INVALID JSON *******" -BackgroundColor Red
      }
 }
-
-
- 
-
